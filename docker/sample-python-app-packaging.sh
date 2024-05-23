9dbcd4e07c48ffde7d37d47aa564ea452634c946
Bobby ABUCHI <bobbyabuchi@gmail.com>
2024-05-23 14:31:04 +0100

    Update and rename sample-python-app-packaging.txt to sample-python-app-packaging.sh

---
+++ docker/sample-python-app-packaging.sh
@@ -0,0 +1,29 @@
+Environment Setup
+Set up your environment to create a Docker image for an application:
+- clone https://github.com/udacity/nd064_course_1 using git
+- nav to exercies/go-helloworld
+- follow the README.md instructions to run and access the app
+-------------------------------------------------------------------------
+Once you can access the application through the local browser, the next steps are to package the application using Docker.
+
+DOIT:
+
+Create the Docker image for the Go web application and push it to DockerHub, considering the following requirements:
+
+Dockerfile:
+- use the golang:alpine base image
+- set the working directory to /go/src/app
+- make sure to copy all the files from the current directory to the container working directory (e.g. /go/src/app)
+- to build the application, use go build -o helloworld command, where -o helloworld will create the binary of the application with the name helloworld
+- the application should be accessible on port 6111
+and lastly, the command to start the container is to invoke the binary created earlier, which is ./helloworld
+Docker image:
+
+should have the name go-helloworld
+should have a valid tag, and a version with a major, minor, and patch included
+should be available in DockerHub, under your username e.g. pixelpotato/go-helloworld
+Docker container:
+
+should be running on your local machine, by referencing the image from the DockerHub with a valid tag e.g. pixelpotato/go-helloworld:v5.12.3
+Note: You will need to use docker login to login into Docker before pushing images to DockerHub.
+
